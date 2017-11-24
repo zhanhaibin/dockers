e86f9d2662797b1e7022a80c2749be9c875e919b
zhanhaibin <enozhiyan@163.com>
2017-11-24 10:52:03 +0800

    update

--- running/nginx/build_dockerfile.sh
+++ running/nginx/build_dockerfile.sh
@@ -13,6 +13,8 @@ TAG=$1
 if [ "${TAG}" == "" ]; then TAG=$(date +%s); fi;
 NAME_TAG=${NAME}:${TAG}
 
+./download_wars.sh
+
 echo 开始构建镜像${NAME_TAG}
 # 调用docker build
 docker build --force-rm --no-cache -f ./dockerfile4nginx -t ${NAME_TAG} ./
