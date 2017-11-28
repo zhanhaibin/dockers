010b329c4386be4b2bfbfbbfdf7e635f80fd1f13
zhanhaibin <enozhiyan@163.com>
2017-11-28 16:59:30 +0800

    修改拷贝文件到maven环境变量目录

--- compiling/maven_typescript/alpine/docker_compile.sh
+++ compiling/maven_typescript/alpine/docker_compile.sh
@@ -37,7 +37,7 @@ docker cp ${WORK_FOLDER}/git-ibas-framework.sh ${DOCKER_ID}:${CODE_HOME}
 docker cp ${WORK_FOLDER}/git-ibas.sh ${DOCKER_ID}:${CODE_HOME}
 docker cp ${WORK_FOLDER}/git-ibas-cloud.sh ${DOCKER_ID}:${CODE_HOME}
 docker cp ${WORK_FOLDER}/compile_cloud_order.txt ${DOCKER_ID}:${CODE_HOME}
-docker cp ${WORK_FOLDER}/settings.xml ${DOCKER_ID}:/usr/share/java/maven-3/conf/
+docker cp ${WORK_FOLDER}/settings.xml ${DOCKER_ID}:${MAVEN_HOME}}/conf/
 
 
 echo --开始运行脚本
