310d6c1dfad4e7234966646f696783fd674eb560
zhanhaibin <enozhiyan@163.com>
2018-01-29 14:44:15 +0800

    update

--- compiling/maven_typescript/alpine/git-ibas-cloud.sh
+++ compiling/maven_typescript/alpine/git-ibas-cloud.sh
@@ -41,7 +41,6 @@ fi
 while read file
 do
 	echo 'Git TF pull： '$file
-  if [ -d ${file} ]; then	rm -r "${file}" fi ;
   git tf clone http://tfs.avatech.com.cn:8080/tfs/ibas_cloud $/"${file}" "${IBAS}${file}"; 
   # 将ibas_cloud的模块写入编译清单
   grep -q "$file" compile_order.txt ||  echo "$file" >> compile_order.txt
